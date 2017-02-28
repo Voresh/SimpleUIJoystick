e86a1c1fbce9e4a25f78d57a384006b998fe55eb
Oreshkov Vladimir <voresh@inbox.ru>
2017-02-28 19:43:21 +0300

    initial commit

---
+++ Assets/SimpleUIJoystick/Scripts/MoveController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets
+{
+    public class MoveController : MonoBehaviour
+    {
+        [SerializeField]
+        private UIJoystick inputJoystick;
+
+        [Range(0.01f, 2f)]
+        [SerializeField]
+        private float speed = 0.01f;
+        [SerializeField]
+        private CharacterController controller;
+        [SerializeField]
+        private Animator cachedUnitAnimator;
+
+        [SerializeField]
+        private string animatorSpeedXName;
+        [SerializeField]
+        private string animatorSpeedZName;
+
+        private Vector3 moveVector;
+
+        public void Awake()
+        {
+            moveVector.y = 0f;
+        }
+
+        public void Update ()
+        {
+            float x = inputJoystick.GetInputX();
+            float z = inputJoystick.GetInputY();
+            cachedUnitAnimator.SetFloat(animatorSpeedXName, Mathf.Abs(x));
+            cachedUnitAnimator.SetFloat(animatorSpeedZName, Mathf.Abs(z));
+            if (Math.Abs(x) > 0 || Math.Abs(z) > 0)
+            {
+                moveVector.x = x;
+                moveVector.z = z;
+                controller.Move(moveVector * speed * Time.deltaTime);
+                transform.forward = moveVector;
+            }
+        }
+    }
+}
---
+++ Assets/SimpleUIJoystick/Scripts/UIJoystick.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets
+{
+    public class UIJoystick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+    {
+        [SerializeField]
+        private RectTransform stickPosition;
+        [SerializeField]
+        private RectTransform circleRectTransform;
+
+        private float circleRange;
+        [SerializeField]
+        private float minStickMoveRange;
+
+        private Vector3 moveVector;
+        private float playerRotationAngle;
+
+        public float GetInputX()
+        {
+            return moveVector.normalized.x;
+        }
+
+        public float GetInputY()
+        {
+            return moveVector.normalized.y;
+        }
+
+        public void Awake()
+        {
+            circleRange = circleRectTransform.sizeDelta.x / 2f;
+        }
+
+        #region touch events
+
+        public void OnPointerDown(PointerEventData data)
+        {
+            PlayerInteractWithJoystick(data.position);
+        }
+
+        public void OnPointerUp(PointerEventData data)
+        {
+            PlayerEndJoystickInteraction();
+        }
+
+        public void OnDrag(PointerEventData data)
+        {
+            PlayerInteractWithJoystick(data.position);
+        }
+
+        #endregion
+
+        private void PlayerEndJoystickInteraction ()
+        {
+            stickPosition.position = circleRectTransform.position;
+            moveVector.x = 0f;
+            moveVector.y = 0f;
+        }
+
+        private void PlayerInteractWithJoystick(Vector3 pos)
+        {
+            moveVector.x = pos.x - circleRectTransform.position.x;
+            moveVector.y = pos.y - circleRectTransform.position.y;
+
+            UpdateJoystick(moveVector);
+
+            if (moveVector.sqrMagnitude < minStickMoveRange * minStickMoveRange)
+            {
+                moveVector.x = 0f;
+                moveVector.y = 0f;
+            }
+        }
+
+        private void UpdateJoystick(Vector3 dir)
+        {
+            if (dir.sqrMagnitude > circleRange * circleRange)
+            {
+                dir = dir.normalized * circleRange;
+            }
+
+            stickPosition.localPosition = dir;
+        }
+    }
+}
